5333cfded8aeedaeb58c297155ce7c3ca9b737b4
BRID19 <74292639+BRID19@users.noreply.github.com>
2025-07-11 04:55:21 +0100

    Auto commit

--- 100-days-of-code/day32-js/script.js
+++
@@ -1,9 +0,0 @@
-let text = "Hello World";
-
-console.log(text.toUpperCase());
-
-console.log(text.indexOf("W"));
-
-console.log(text.slice(0, 5));
-
-console.log(text.replace("World", "JavaScript"));
