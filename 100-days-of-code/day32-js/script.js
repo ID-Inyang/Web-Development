dde96d06eac07697945419c5506e16448dbb592c
BRID19 <74292639+BRID19@users.noreply.github.com>
2025-07-11 19:20:59 +0100

    Auto commit

--- 100-days-of-code/day32-js/script.js
+++ 100-days-of-code/day32-js/script.js
@@ -1,11 +1,1 @@
-const grade = 15;
-
-if (grade >= 90) {
-    console.log('A grade - Excellent!');
-} else if (grade >= 80) {
-    console.log("B grade - Good job!");
-} else if (grade >= 70) {
-    console.log('C grade - You passed!');
-} else {
-    console.log('Need to study more!');
-}
+const age = 25
