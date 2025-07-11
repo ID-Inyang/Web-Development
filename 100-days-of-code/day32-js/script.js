cea9810e26dadaf6666dcd97483065d53a322597
BRID19 <74292639+BRID19@users.noreply.github.com>
2025-07-11 19:34:39 +0100

    Auto commit

--- 100-days-of-code/day32-js/script.js
+++ 100-days-of-code/day32-js/script.js
@@ -3,5 +3,7 @@ const scores = [50, 25, 0, 100, 20, 10];
 for (let i = 0; i < scores.length; i++) {
     console.log('Score:', scores[i]);
 
-    if (scores[i] === 100)
+    if (scores[i] === 100) {
+        
+    }
 }
