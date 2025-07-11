8c0608ec795019b0a486a4342f686bfdf8f87d71
BRID19 <74292639+BRID19@users.noreply.github.com>
2025-07-11 19:52:46 +0100

    Auto commit

--- 100-days-of-code/day32-js/script.js
+++ 100-days-of-code/day32-js/script.js
@@ -23,7 +23,7 @@ if (age >= 18) {
 }
 
 // 4. String Methods
-let email = "e@gmail.com";
+let email = "@gmail.com";
 console.log("Email in uppercase:", email.toUpperCase());
 console.log("@ symbol position:", email.indexOf('@'));
 console.log("Domain:", email.slice(email.indexOf('@') + 1));
