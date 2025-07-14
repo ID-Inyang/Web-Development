97b439cf5b8822467519ec421c8c2da0e1a40ef6
BRID19 <74292639+BRID19@users.noreply.github.com>
2025-07-14 18:36:01 +0100

    Auto commit

--- 100-days-of-code/day34-js/using-variables.js
+++ 100-days-of-code/day34-js/using-variables.js
@@ -6,7 +6,7 @@ var interestRate;
 interestRate = .03;
 // document.write(interestRate);
 
-let message = "The double quotation mark (\") encloses this string.\rr THE";
+let message = "The double quotation mark (\") encloses this string.\r\b THE";
 document.write(message);
 
 var hexValue = '';
