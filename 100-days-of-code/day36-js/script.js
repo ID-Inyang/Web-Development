2c1f26f2465788bd4e3b82cb232ae7ea804dd356
BRID19 <74292639+BRID19@users.noreply.github.com>
2025-07-15 18:18:14 +0100

    Auto commit

--- 100-days-of-code/day36-js/script.js
+++ 100-days-of-code/day36-js/script.js
@@ -3,4 +3,4 @@ var tipAmount = preTipTotal * 0.15;
 var message1 = "Your tip is ";
 var message2 = "\nYour total bill is ";
 var totalBill = preTipTotal + tipAmount;
-alert(message1 + tipAmount + message2 + totalBill);
+// alert(message1 + tipAmount + message2 + totalBill);
