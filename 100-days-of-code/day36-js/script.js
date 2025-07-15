2ddb087fe91c9036d82c3d58b360622c716ea3b8
BRID19 <74292639+BRID19@users.noreply.github.com>
2025-07-15 18:48:43 +0100

    Auto commit

--- 100-days-of-code/day36-js/script.js
+++ 100-days-of-code/day36-js/script.js
@@ -4,4 +4,4 @@ var v3 = "testing";
 var v4 = false;
 var v5 = 0;
 var v6 = "";
-var leftOperand = eval(prompt("Enter the left operand (a value or expression):", tr))
+var leftOperand = eval(prompt("Enter the left operand (a value or expression):", true));
