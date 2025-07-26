e5e3965238c9c5beeb90d787c49b3b6e8e798043
BRID19 <74292639+BRID19@users.noreply.github.com>
2025-07-26 11:34:43 +0100

    Auto commit

--- 100-days-of-code/day44/script.js
+++ 100-days-of-code/day44/script.js
@@ -15,7 +15,7 @@ console.log(input);
 
 let buttonTag = document.getElementsByTagName("button");
 // buttonTag[0].innerText = "DOM button update";
-buttonTag
+buttonTag[0].onc
 
 function showAlert(userName){
     alert("Welcome to UpSkill " + userName);
