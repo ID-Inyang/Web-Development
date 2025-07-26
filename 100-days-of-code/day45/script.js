fea0978750eb081c62b052315bf746a3216d3407
BRID19 <74292639+BRID19@users.noreply.github.com>
2025-07-26 15:08:45 +0100

    Auto commit

--- 100-days-of-code/day45/script.js
+++ 100-days-of-code/day45/script.js
@@ -6,7 +6,7 @@ console.log(button);
 
 let input = document.getElementById("inputId");
 
-let name = input.val
+let name = input.ariaValueMax;
 
 button.addEventListener("click", function () {
     dummyText.innerText = "This has been replaced by the real content";
