b0944a54b71223744e13ac3ca4a0447b01e8e4c7
BRID19 <74292639+BRID19@users.noreply.github.com>
2025-07-26 14:29:24 +0100

    Auto commit

--- 100-days-of-code/day45/script.js
+++ 100-days-of-code/day45/script.js
@@ -6,4 +6,4 @@ let button = document.getElementById("btn");
 console.log(button);
 // dummyText.innerText = "This has been replaced by the real content";
 
-button.addEventListener("click")
+button.addEventListener("click", function)
