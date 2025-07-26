a306815432f3eb1ab475cde13c1de0cdd0c600a1
BRID19 <74292639+BRID19@users.noreply.github.com>
2025-07-26 16:48:42 +0100

    Auto commit

--- 100-days-of-code/day45/scriptTwo.js
+++ 100-days-of-code/day45/scriptTwo.js
@@ -3,5 +3,5 @@ let textArea = document.getElementById("textArea");
 let inputID = document.getElementById("inputId");
 
 inputID.addEventListener("input", function () {
-    textArea.innerHTML = inputID.value;
+    textArea.inneT = inputID.value;
 })
