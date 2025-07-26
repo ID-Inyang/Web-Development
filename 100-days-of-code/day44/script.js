4cfa9e2da951242244b1bbb70c6e49ef42deb836
BRID19 <74292639+BRID19@users.noreply.github.com>
2025-07-26 11:23:16 +0100

    Auto commit

--- 100-days-of-code/day44/script.js
+++ 100-days-of-code/day44/script.js
@@ -4,3 +4,16 @@ myTitle.innerText = "This is the DOM heading";
 console.log(myTitle);
 
 let paragraphClass = document.getElementsByClassName("paragraph");
+console.log(paragraphClass[0]);
+
+paragraphClass[0].textContent = "This paragraph has been changed by the DOM";
+paragraphClass[0].style.color = 'lightblue';
+
+let input = document.getElementById("inputId");
+input.value = 'Enter your name';
+
+let buttonTag = document.getElementsByTagName("button");
+
+function showAlert(){
+    alert("Welcome to UpSkill " + userName);
+}
