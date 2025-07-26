e9e152f636816fa1539ad78131babc2dba69cb0f
BRID19 <74292639+BRID19@users.noreply.github.com>
2025-07-26 17:26:57 +0100

    Auto commit

---
+++ 100-days-of-code/day45/scriptTwo.js
@@ -0,0 +1,16 @@
+let textArea = document.getElementById("textArea");
+
+let inputID = document.getElementById("inputID");
+
+let p = document.getElementById("p");
+
+inputID.addEventListener("input", function () {
+    textArea.innerText = inputID.value;
+
+    let count = inputID.value.length;
+    p.innerText = count;
+
+    if (count >= 20) {
+        alert("You have reached the ")
+    }
+})
