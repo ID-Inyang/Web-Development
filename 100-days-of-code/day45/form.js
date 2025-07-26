cc8c17e48ddb2af39693748faf68e7054ab1ae75
BRID19 <74292639+BRID19@users.noreply.github.com>
2025-07-26 22:11:48 +0100

    Auto commit

--- 100-days-of-code/day45/form.js
+++ 100-days-of-code/day45/form.js
@@ -9,6 +9,6 @@ myForm.addEventListener("submit", function (e) {
         alert("The Email is Valid");
     }
     else {
-        alert("The Email is no")
+        alert("The Email is not ")
     }
 })
