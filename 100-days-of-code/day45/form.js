63864f7cb5ca3775e6aa0f3de656506800dda220
BRID19 <74292639+BRID19@users.noreply.github.com>
2025-07-26 22:01:21 +0100

    Auto commit

--- 100-days-of-code/day45/form.js
+++ 100-days-of-code/day45/form.js
@@ -1,4 +1,4 @@
-let mtyForm = document.getElementById("myform");
+let myForm = document.getElementById("myform");
 let emailText = document.getElementById("emailText");
 
 myForm.addEventListener("submit", function (e) {
