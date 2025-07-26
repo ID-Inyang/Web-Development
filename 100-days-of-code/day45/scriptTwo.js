4531ea0684af838b9446173d946ed4129c84ad95
BRID19 <74292639+BRID19@users.noreply.github.com>
2025-07-26 17:27:23 +0100

    Auto commit

--- 100-days-of-code/day45/scriptTwo.js
+++ 100-days-of-code/day45/scriptTwo.js
@@ -11,6 +11,6 @@ inputID.addEventListener("input", function () {
     p.innerText = count;
 
     if (count >= 20) {
-        alert("You have reached the limit")
+        alert("You can only type a ")
     }
 })
